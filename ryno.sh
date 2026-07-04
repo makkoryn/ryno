ff1dcb9785e18a717d0d2c20efea04088211a49b
makkoryn <34269556+makkoryn@users.noreply.github.com>
2026-07-04 15:53:06 -0500

    Style and logic modifications

--- ryno.sh
+++ ryno.sh
@@ -10,57 +10,61 @@
 # Version: 		0.2.0
 # =========================================================================
 
-# Init variables
+# Init global variables
 WAN_INTERFACE="em0"
 SLEEP_TIMER="10m"
-fail_tracker=0
-log_message=""
+FAIL_TRACKER=0
+LOG_MESSAGE=""
 
 send_to_log(){
-	log_message=$1
-	logger "$log_message"
+	LOG_MESSAGE=$1
+	logger "$LOG_MESSAGE"
 }
 
-while true;
-do 
-	# Check if we can connect to Quad9 DNS (9.9.9.9) on port 53
-	# If we are successful, we're online
-	if nc -z -n -v -w 3 9.9.9.9 53 2>&1 | grep succeeded; then
-		# Log attempt as successful
-		send_to_log "[ryno.sh] | SUCCESS | WAN interface ($WAN_INTERFACE) connected to 9.9.9.9:53"
-	# If we can't, assume we're offline
-	else
-		# Log failure attempt
-		send_to_log "[ryno.sh] | FAIL | WAN interface ($WAN_INTERFACE) unable to connect to 9.9.9.9:53"
-		sleep 1
-		# Bring WAN interface down
-		send_to_log "[ryno.sh] | INFO | Bringing WAN interface ($WAN_INTERFACE) down!"
-		ifconfig $WAN_INTERFACE down
-		sleep 1
-		# Clear existing leases
-		send_to_log "[ryno.sh] | INFO | Removing existing dhclient leases..."
-		rm /var/db/dhclient.leases.*
-		sleep 1
-		# Bring WAN interface up
-		send_to_log "[ryno.sh] | INFO | Bringing WAN interface ($WAN_INTERFACE) up!"
-		ifconfig $WAN_INTERFACE up
-		sleep 1
-		# Request a new DHCP Lease
-		if dhclient -cf /var/etc/dhclient_wan.conf $WAN_INTERFACE; then
-			send_to_log "[ryno.sh] | SUCCESS | New DHCP Lease requested on WAN interface ($WAN_INTERFACE)"
+main(){
+	while true;
+	do 
+		# Check if we can connect to Quad9 DNS (9.9.9.9) on port 53
+		# If we are successful, we're online
+		if nc -z -n -v -w 3 9.9.9.9 53 2>&1 | grep succeeded; then
+			# Log attempt as successful
+			send_to_log "[ryno.sh] | SUCCESS | WAN interface ($WAN_INTERFACE) connected to 9.9.9.9:53"
+		# If we can't, assume we're offline
 		else
-			send_to_log "[ryno.sh] | FAIL | Failed to request a new DHCP Lease on WAN interface ($WAN_INTERFACE). Check physical connection?"
-			fail_tracker=$($fail_tracker + 1)
+			# Log failure attempt
+			send_to_log "[ryno.sh] | FAIL | WAN interface ($WAN_INTERFACE) unable to connect to 9.9.9.9:53"
+			sleep 1
+			# Bring WAN interface down
+			send_to_log "[ryno.sh] | INFO | Bringing WAN interface ($WAN_INTERFACE) down!"
+			ifconfig $WAN_INTERFACE down
+			sleep 1
+			# Clear existing leases
+			send_to_log "[ryno.sh] | INFO | Removing existing dhclient leases..."
+			rm /var/db/dhclient.leases.*
+			sleep 1
+			# Bring WAN interface up
+			send_to_log "[ryno.sh] | INFO | Bringing WAN interface ($WAN_INTERFACE) up!"
+			ifconfig $WAN_INTERFACE up
+			sleep 1
+			# Request a new DHCP Lease
+			if dhclient -cf /var/etc/dhclient_wan.conf $WAN_INTERFACE; then
+				send_to_log "[ryno.sh] | SUCCESS | New DHCP Lease requested on WAN interface ($WAN_INTERFACE)"
+			else
+				send_to_log "[ryno.sh] | FAIL | Failed to request a new DHCP Lease on WAN interface ($WAN_INTERFACE). Check physical connection?"
+				FAIL_TRACKER=$($FAIL_TRACKER + 1)
+			fi
+			sleep 1
 		fi
-		sleep 1
-	fi
-	# We track how many times the system fails to obtain a new DHCP Lease.
-	# After 6 attempts (approx. 1 hour down time by default), we assume ISP is up, but dhclient isn't working, and reboot the system.
-	if $fail_tracker -ge 6; then
-		echo "[ryno.sh] | FAIL | The system has failed to obtain a new DHCP Lease. Rebooting..." | logger
-		fail_tracker=0
-		shutdown -r now
-	fi
-	# Wait before trying again (default: 10 minutes)
-	sleep $SLEEP_TIMER
-done
+		# We track how many times the system fails to obtain a new DHCP Lease.
+		# After 6 attempts (approx. 1 hour down time by default), we assume ISP is up, but dhclient isn't working, and reboot the system.
+		if $FAIL_TRACKER -ge 6; then
+			echo "[ryno.sh] | FAIL | The system has failed to obtain a new DHCP Lease. Rebooting..." | logger
+			FAIL_TRACKER=0
+			shutdown -r now
+		fi
+		# Wait before trying again (default: 10 minutes)
+		sleep $SLEEP_TIMER
+	done
+}
+
+main
