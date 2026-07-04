fafaec7e5f596b73647607720f3ed84eda7d1403
makkoryn <34269556+makkoryn@users.noreply.github.com>
2026-07-04 16:36:36 -0500

    Info comments, readability

--- ryno.sh
+++ ryno.sh
@@ -7,13 +7,14 @@
 #				the pfSense WAN interface if a connection attempt fails.
 # Author: 		makkoryn (github.com/makkoryn)
 # Date: 		2026-07-02
-# Version: 		0.2.0
+# Version: 		0.2.2
 # =============================================================================
 
 # Init global variables
+# WAN_INTERFACE must reflect the name of the network port assigned to the WAN interface
 WAN_INTERFACE="em0"
+# SLEEP_TIMER is how often the main loop should be run.
 SLEEP_TIMER="10m"
-FAIL_TRACKER=0
 
 send_to_log(){
 	log_message=$1
@@ -21,6 +22,7 @@ send_to_log(){
 }
 
 main(){
+	fail_tracker=0
 	while true;
 	do 
 		# Check if we can connect to Quad9 DNS (9.9.9.9) on port 53
@@ -50,16 +52,16 @@ main(){
 				send_to_log "[ryno.sh] | SUCCESS | New DHCP Lease requested on WAN interface ($WAN_INTERFACE)"
 			else
 				send_to_log "[ryno.sh] | FAIL | Failed to request a new DHCP Lease on WAN interface ($WAN_INTERFACE). Check physical connection?"
-				FAIL_TRACKER=$($FAIL_TRACKER + 1)
+				fail_tracker=$($fail_tracker + 1)
 			fi
 			sleep 1
 		fi
 		# We track how many times the system fails to obtain a new DHCP Lease.
 		# After 6 attempts (approx. 1 hour down time by default), assume ISP is
 		# up, but dhclient isn't working, and reboot the system.
-		if $FAIL_TRACKER -ge 6; then
+		if $fail_tracker -ge 6; then
 			send_to_log "[ryno.sh] | FAIL | The system has failed to obtain a new DHCP Lease. Rebooting..."
-			FAIL_TRACKER=0
+			fail_tracker=0
 			shutdown -r now
 		fi
 		# Wait before trying again (default: 10 minutes)
